345498ee3fe4995ea0d63618bb674d8e4b24c699
englsergio <englsergio@users.noreply.github.com>
2022-07-08 22:46:26 -0300

    Deletando arquivos salvos no dir errado

--- SpringBoot DIO/projeto-spring-api/src/main/java/com/example/projetospringapi/model/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.projetospringapi.model;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientRepository extends CrudRepository<Client, Long> {
-}
